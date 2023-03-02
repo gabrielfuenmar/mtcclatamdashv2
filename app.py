a325eafa21ee5dab937909ac61882a4a2329d85d
Gabriel Fuentes <45942967+gabrielfuenmar@users.noreply.github.com>
2023-03-02 23:09:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -51,8 +51,11 @@ for file in bucket_list:
           
         last_update=datetime.fromisoformat(file.split("/")[-1].split("&")[-1].split(".")[0]).strftime("%d %b %Y")
         
-        ###1 month before or first position
-        fr_update=(datetime.now()-relativedelta(months=1)).replace(day=1)
+        ###20 months before or first position
+        fr_update=(datetime.fromisoformat(file.split("/")[-1].split("&")[-1].split(".")[0])-relativedelta(months=20)).replace(day=1)
+
+        if fr_update<datetime.fromisoformat("2022-12-01"):
+            fr_update=datetime.fromisoformat("2022-12-01")
                   
         to_update=datetime.fromisoformat(file.split("/")[-1].split("&")[-1].split(".")[0])
         
